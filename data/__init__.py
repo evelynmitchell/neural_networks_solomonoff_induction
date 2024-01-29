7ccbebbe51da5314268082bea5681763de5a27b9
evelynmitchell <efmphone@gmail.com>
2024-01-29 13:56:02 -0700

    added data init

---
+++ data/__init__.py
@@ -0,0 +1,16 @@
+
+import chomsky_data_generator as cdg
+import ctw_data_generator as ctwdg
+import data_generator as dg
+import meta_data_generator as mdg
+import utm_data_generator as utmdg
+import utms
+
+__all__ = [
+    ChomskyDataGenerator,
+    CTWGenerator,
+    DataGenerator,
+    MetaDataGenerator,
+    UTMDataGenerator,
+    BrainPhoqueUTM,
+]
